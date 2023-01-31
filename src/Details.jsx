fcfb8352af0aee39497ee478d6d42a5a5bff8e0e
Volodymyr Kochergin <volodymyrkochergin@Volodymyrs-MacBook-Pro.local>
2023-01-31 14:43:24 +0100

    add error handling

--- src/Details.jsx
+++ src/Details.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel.jsx";
+import ErrorBoundary from "./ErrorBoundary.jsx";
 
 const Details = () => {
   const { id } = useParams();
@@ -32,4 +33,10 @@ const Details = () => {
   );
 };
 
-export default Details;
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props}/>
+    </ErrorBoundary>
+  );
+}
