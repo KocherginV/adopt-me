b48703426ad4ac8567ded1b76711065b1c69ad02
Volodymyr Kochergin <volodymyrkochergin@gmail.com>
2023-02-02 11:20:02 +0100

    add context

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
+import { useState } from "react";
 import SearchParams from "./SearchParams.jsx";
+import AdoptedPetContext from "./AdoptedPetContext.js";
 import { Link, BrowserRouter, Route, Routes } from "react-router-dom";
 import Details from "./Details.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -14,16 +16,19 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+  const adoptedPet = useState(null);
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </AdoptedPetContext.Provider>
       </QueryClientProvider>
     </BrowserRouter>
   );
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,13 +1,17 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel.jsx";
 import ErrorBoundary from "./ErrorBoundary.jsx";
 import Modal from "./Modal.jsx";
+import AdoptedPetContext from "./AdoptedPetContext";
 
 const Details = () => {
   const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
+  // eslint-disable-next-line no-unused-vars
+  const [_, setAdoptedPet] = useContext(AdoptedPetContext);
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
 
@@ -36,7 +40,14 @@ const Details = () => {
             <div>
               <h1>Would you like to adopt {pet.name}?</h1>
               <div className="buttons">
-                <button>Yes</button>
+                <button
+                  onClick={() => {
+                    setAdoptedPet(pet);
+                    navigate("/");
+                  }}
+                >
+                  Yes
+                </button>
                 <button onClick={() => setShowModal(false)}>No</button>
               </div>
             </div>
